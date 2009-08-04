02b344789bd43d2859f8786a7a24cc9476d278c7
vaclav.pech <vaclav.pech@390304ce-b589-11dd-818f-3bd198e1d18e>
2009-08-04 07:27:36 +0000

    Updated
    git-svn-id: https://gparallelizer.googlecode.com/svn/trunk@426 390304ce-b589-11dd-818f-3bd198e1d18e

--- src/test/groovy/org/gparallelizer/dataflow/DataFlowStreamTest.groovy
+++ src/test/groovy/org/gparallelizer/dataflow/DataFlowStreamTest.groovy
@@ -29,6 +29,29 @@ public class DataFlowStreamTest extends GroovyTestCase {
         assertEquals 0, stream.length()
     }
 
+    public void testNullValues() {
+        final CountDownLatch latch = new CountDownLatch(1)
+
+        final DataFlowStream stream = new DataFlowStream()
+        final AbstractPooledActor thread = DataFlow.thread {
+            stream << null
+            final DataFlowVariable variable = new DataFlowVariable()
+            stream << variable
+            latch.countDown()
+            react {
+                variable << null
+            }
+        }
+
+        latch.await()
+        assertEquals 2, stream.length()
+        assertEquals null, stream.val
+        assertEquals 1, stream.length()
+        thread << 'Proceed'
+        assertEquals null, stream.val
+        assertEquals 0, stream.length()
+    }
+
     public void testTake() {
         final CountDownLatch latch = new CountDownLatch(1)
 
@@ -75,6 +98,23 @@ public class DataFlowStreamTest extends GroovyTestCase {
         (0..10).each {
             assertEquals it, stream.val
         }
+    }
+
+    public void testIterationWithNulls() {
+        final CyclicBarrier barrier = new CyclicBarrier(2)
+
+        final DataFlowStream stream = new DataFlowStream()
+        DataFlow.thread {
+            (0..10).each {stream << null}
+            barrier.await()
+        }
+
+        barrier.await()
+        assertEquals 11, stream.length()
+        stream.each {assertNull it }
+        assertEquals 11, stream.length()
+
+        for(i in (0..10)) { assertNull stream.val }
     }
 
     public void testToString() {
