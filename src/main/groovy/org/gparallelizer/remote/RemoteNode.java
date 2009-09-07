d8b41465aa336acec6d34d2fe8b5feff7c4580be
alex.tkachman@gmail.com <alex.tkachman@gmail.com@390304ce-b589-11dd-818f-3bd198e1d18e>
2009-09-07 19:07:54 +0000

    distributed actors -> NonSharedMemory
    git-svn-id: https://gparallelizer.googlecode.com/svn/trunk@521 390304ce-b589-11dd-818f-3bd198e1d18e

--- src/main/groovy/org/gparallelizer/remote/RemoteNode.java
+++ src/main/groovy/org/gparallelizer/remote/RemoteNode.java
@@ -1,9 +1,9 @@
 package org.gparallelizer.remote;
 
 import org.gparallelizer.actors.ActorMessage;
-import org.gparallelizer.remote.sharedmemory.SharedMemoryNode;
+import org.gparallelizer.actors.Actor;
 
-import java.io.Serializable;
+import java.io.*;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -13,18 +13,22 @@ import java.util.concurrent.ConcurrentHashMap;
 public abstract class RemoteNode {
     public final static UUID MAIN_ACTOR_ID = new UUID(0L,0L);
 
-    private final static RemoteActor DUMMY = new RemoteActor(null);
+    protected final static RemoteActor DUMMY = new RemoteActor(null,null);
 
+    // TODO: DANGER of memory leak
     private final ConcurrentHashMap<UUID,RemoteActor> remoteActors = new ConcurrentHashMap<UUID,RemoteActor> ();
+
+    // TODO: DANGER of memory leak
+    private final ConcurrentHashMap<Actor,UUID> localActors = new ConcurrentHashMap<Actor, UUID>();
+
+    protected final ConcurrentHashMap<UUID,Actor> localActorsId = new ConcurrentHashMap<UUID, Actor>();
 
     public RemoteNode() {
     }
 
     public abstract UUID getId ();
 
-    public abstract void send(RemoteActor receiver, ActorMessage<Serializable> message);
-
-    public abstract void onDisconnect(SharedMemoryNode smn);
+    public abstract void onDisconnect(RemoteNode smn);
 
     @Override
     public String toString() {
@@ -54,4 +58,59 @@ public abstract class RemoteNode {
     }
 
     protected abstract RemoteActor createRemoteActor(UUID uid);
+
+    protected abstract void deliver(byte[] bytes) throws IOException;
+
+    protected UUID getLocalActorId (Actor actor) {
+        if (actor == null) {
+            return null;
+        }
+
+        UUID uid = localActors.putIfAbsent(actor, MAIN_ACTOR_ID);
+        if (uid == null || uid == MAIN_ACTOR_ID) {
+            synchronized (localActors) {
+                uid = localActors.get(actor);
+                if (uid == null || uid == MAIN_ACTOR_ID) {
+                    uid = UUID.randomUUID();
+                    localActors.put(actor, uid);
+                    localActorsId.put(uid, actor);
+                }
+            }
+        }
+        return uid;
+    }
+
+    protected void send(RemoteActor receiver, ActorMessage<Serializable> message) {
+        try {
+           byte [] data = encodeMessage(receiver, message);
+           deliver(data);
+        }
+        catch (IOException e) { //
+        }
+    }
+
+    protected byte[] encodeMessage(RemoteActor receiver, ActorMessage<Serializable> message) throws IOException {
+        final RemoteMessage toSend = new RemoteMessage(receiver.getId(), getLocalActorId(message.getSender()), message.getPayLoad());
+        final ByteArrayOutputStream bout = new ByteArrayOutputStream();
+        try {
+            final ObjectOutputStream oout = new ObjectOutputStream(bout);
+            oout.writeObject(toSend);
+            oout.close();
+            return bout.toByteArray();
+        } catch (IOException e) {
+            throw e;
+        }
+    }
+
+    protected RemoteMessage decodeMessage (byte [] data) {
+        try {
+            return (RemoteMessage) new ObjectInputStream(new ByteArrayInputStream(data)).readObject();
+        } catch (Throwable e) {
+            return null;
+        }
+    }
+
+    public Actor getLocalActor(UUID uuid) {
+        return localActorsId.get(uuid);
+    }
 }
