07b8a6253b6a5e3406dcf6d084331fe231d4ff44
russel.winder@concertant.com <russel.winder@concertant.com@390304ce-b589-11dd-818f-3bd198e1d18e>
2009-09-14 18:36:48 +0000

    Fix the ISO-8859-1 encoding to UTF-8.
    git-svn-id: https://gparallelizer.googlecode.com/svn/trunk@620 390304ce-b589-11dd-818f-3bd198e1d18e

--- src/main/groovy/org/gparallelizer/remote/messages/NodeDisconnectedMsg.java
+++ src/main/groovy/org/gparallelizer/remote/messages/NodeDisconnectedMsg.java
@@ -1,6 +1,6 @@
 //  GParallelizer
 //
-//  Copyright © 2008-9  The original author or authors
+//  Copyright Â© 2008-9  The original author or authors
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
