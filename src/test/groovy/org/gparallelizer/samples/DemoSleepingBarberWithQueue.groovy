bf7e30467cde5c23d8df1557e11437b8696ab8f2
dierk.koenig@canoo.com <dierk.koenig@canoo.com@390304ce-b589-11dd-818f-3bd198e1d18e>
2009-09-24 20:41:33 +0000

    added documentation
    git-svn-id: https://gparallelizer.googlecode.com/svn/trunk@731 390304ce-b589-11dd-818f-3bd198e1d18e

--- src/test/groovy/org/gparallelizer/samples/DemoSleepingBarberWithQueue.groovy
+++ src/test/groovy/org/gparallelizer/samples/DemoSleepingBarberWithQueue.groovy
@@ -1,21 +1,34 @@
 package org.gparallelizer.samples
 
-import java.util.concurrent.ArrayBlockingQueue as Queue
+import java.util.concurrent.ArrayBlockingQueue
 import org.gparallelizer.actor.impl.AbstractPooledActor
 
-def shaveTime   = 100
-def random      = new Random()
-def seatCount   = 3
-def fairAccess  = true
-def seats       = new Queue(seatCount, fairAccess)
+/*
+Demo for solving the classic sleeping barber problem with the help
+of core Java features (blocking queue) in combination with gpars' actors that
+care for putting queue operations and business operations into
+proper uninterrupted sequence.
+The println of sleeping, taking seats, leaving, and starting/stopping the shave
+are placeholders for business operations.
+@author Dierk Koenig, Vaclav Pech
+*/
 
+/** The constants that define the world context of the barber shop.*/
+class Shop {
+    static final shaveTime   = 100
+    static final seatCount   = 3
+    static final fairAccess  = true
+    static final seats       = new ArrayBlockingQueue(seatCount, fairAccess)
+}
+
+/** The active element that makes sure that all actions happen in proper
+ * uninterrupted sequence, e.g. taking a seat before getting a shave.
+ */
 class Customer extends AbstractPooledActor {
     int id
-    Queue seats
-    int shaveTime
 
     void act() {
-        def result = seats.offer(this)
+        def result = Shop.seats.offer(this)
         if (!result) {
             println "customer $id leaves since no seat is available"
             return
@@ -23,23 +36,30 @@ class Customer extends AbstractPooledActor {
         println "customer $id is taking a seat"
         react {
             println "customer $id get's a shave"
-            sleep shaveTime
+            sleep Shop.shaveTime
             reply "customer $id shaved"
         }
     }
 }
 
+/**
+ * The barber serves its customers and is therefore a classic daemon thread.
+ */
 def barber = Thread.startDaemon {
+    final NO_MESSAGE = null
 	while(true) {
-		if (seats.empty) println "sleeping"
-		println seats.take().sendAndWait(null)
+        // here is a gotcha if a customer enters the shop after the empty check but before the println!
+		if (Shop.seats.empty) println "sleeping"
+		println Shop.seats.take().sendAndWait(NO_MESSAGE)
 	}
 }
 
-def customers = []
-15.times {
-    customers << new Customer(id:it, seats:seats, shaveTime: shaveTime).start()
-    sleep random.nextInt(shaveTime)
+// start 15 customer actors with some delay to get the simulation running.
+def customers = (1..15).collect { new Customer(id:it) }
+def random = new Random()
+for (customer in customers) {
+    customer.start()
+    sleep random.nextInt(Shop.shaveTime)
 }
-
+// make sure the simulation ends after all customers visited the shop
 customers*.join()
