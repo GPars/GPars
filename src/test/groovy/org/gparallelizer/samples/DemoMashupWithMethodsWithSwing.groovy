6e1a1f3db016ec352302d504a3956b4af619cae5
vaclav.pech <vaclav.pech@390304ce-b589-11dd-818f-3bd198e1d18e>
2009-09-29 07:43:58 +0000

    Moved the Swing demo
    git-svn-id: https://gparallelizer.googlecode.com/svn/trunk@761 390304ce-b589-11dd-818f-3bd198e1d18e

--- src/test/groovy/org/gparallelizer/samples/DemoMashupWithMethodsWithSwing.groovy
+++ src/test/groovy/org/gparallelizer/samples/DemoMashupWithMethodsWithSwing.groovy
@@ -14,7 +14,7 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License. 
 
-package org.gparallelizer.samples.dataflow
+package org.gparallelizer.samples
 
 import groovy.swing.SwingBuilder
 import javax.swing.JFrame
