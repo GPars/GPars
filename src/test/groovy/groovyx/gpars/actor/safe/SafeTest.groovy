8fca9c98ce9d6dc619baba2569e3ecbe884b011d
vaclav.pech <vaclav.pech@390304ce-b589-11dd-818f-3bd198e1d18e>
2009-10-04 18:02:14 +0000

    Renamed the dafevariable package
    git-svn-id: https://gparallelizer.googlecode.com/svn/trunk@814 390304ce-b589-11dd-818f-3bd198e1d18e

--- src/test/groovy/groovyx/gpars/actor/safe/SafeTest.groovy
+++ src/test/groovy/groovyx/gpars/actor/safe/SafeTest.groovy
@@ -14,7 +14,7 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License. 
 
-package groovyx.gpars.actor.safevariable
+package groovyx.gpars.actor.safe
 
 import java.util.concurrent.atomic.AtomicBoolean
 import groovyx.gpars.actor.Actors
