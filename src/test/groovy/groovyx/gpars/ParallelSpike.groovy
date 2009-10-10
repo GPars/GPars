6dbdce76e3eae67bfc1aa4bc231c67e2a72ae568
dierk.koenig@canoo.com <dierk.koenig@canoo.com@390304ce-b589-11dd-818f-3bd198e1d18e>
2009-10-10 15:11:06 +0000

    only a spike - just to get the discussion rolling
    git-svn-id: https://gparallelizer.googlecode.com/svn/trunk@866 390304ce-b589-11dd-818f-3bd198e1d18e

---
+++ src/test/groovy/groovyx/gpars/ParallelSpike.groovy
@@ -0,0 +1,32 @@
+package groovyx.gpars
+
+// only a spike - just to get the discussion rolling
+// Author: Dierk Koenig
+
+class Parallel {
+  @Delegate adaptee
+
+  def each(Closure yield) { Parallelizer.withParallelizer { adaptee.eachAsync(yield) } }
+
+  static void prepare(obj) {
+    obj.metaClass.getParallel = {->
+      new Parallel(adaptee: delegate)
+    }
+  }
+}
+
+// works on objects
+def obj = (1..6).toList()
+Parallel.prepare obj
+
+def threadPrinter = { println Thread.currentThread() }
+
+obj.each threadPrinter
+obj.parallel.each threadPrinter
+obj.each threadPrinter
+
+// but just as well on classes or interfaces
+Parallel.prepare Set
+def set = (1..6) as Set
+set.parallel.each threadPrinter
+set.each threadPrinter
