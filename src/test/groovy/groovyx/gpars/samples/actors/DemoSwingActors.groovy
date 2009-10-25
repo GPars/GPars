eca6f162a2912eb19f7959326474a78178d9ba54
vaclav.pech <vaclav.pech@390304ce-b589-11dd-818f-3bd198e1d18e>
2009-10-25 19:40:45 +0000

    Added license for the new demos
    git-svn-id: https://gparallelizer.googlecode.com/svn/trunk@914 390304ce-b589-11dd-818f-3bd198e1d18e

--- src/test/groovy/groovyx/gpars/samples/actors/DemoSwingActors.groovy
+++ src/test/groovy/groovyx/gpars/samples/actors/DemoSwingActors.groovy
@@ -1,3 +1,19 @@
+//  GPars (formerly GParallelizer)
+//
+//  Copyright © 2008-9  The original author or authors
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
 package groovyx.gpars.samples.actors
 
 import groovy.swing.SwingBuilder
@@ -6,6 +22,10 @@ import java.awt.Font
 import javax.swing.JFrame
 import javax.swing.JTextArea
 import javax.swing.SwingUtilities
+
+/**
+ * @author Vaclav Pech
+ */
 
 @Immutable final class WordFindRequest {
     String site
