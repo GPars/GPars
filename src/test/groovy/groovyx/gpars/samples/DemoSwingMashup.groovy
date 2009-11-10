21b38e9d8f77a515678910b91d9c8dd29d8d62fd
vaclav.pech <vaclav.pech@390304ce-b589-11dd-818f-3bd198e1d18e>
2009-11-10 16:36:47 +0000

    Updated the layout of the demo
    git-svn-id: https://gparallelizer.googlecode.com/svn/trunk@960 390304ce-b589-11dd-818f-3bd198e1d18e

--- src/test/groovy/groovyx/gpars/samples/DemoSwingMashup.groovy
+++ src/test/groovy/groovyx/gpars/samples/DemoSwingMashup.groovy
@@ -36,27 +36,35 @@ final def threadPoolSize = 2  //feel free to play around with various values
 
 final def frame = new SwingBuilder().frame(title: 'Demo', defaultCloseOperation: JFrame.EXIT_ON_CLOSE) {
     vbox() {
-        label 'Sites:'
+        panel() {
+            gridLayout()
+            label 'Sites:'
+        }
         scrollPane() {
             textArea(columns: 80, rows: 20, id: 'result')
         }
-        label 'Log:'
+        panel() {
+            gridLayout()
+            label 'Log:'
+        }
         scrollPane() {
             textArea(columns: 80, rows: 25, id: 'logMessages')
         }
-        button('Click', actionPerformed: {
-            doOutside {
-                urls.each {url ->
-                    edt {logMessages.text += "Started downloading from $url \n"}
-                    def content = url.toURL().text
-                    edt {logMessages.text += "Done downloading from $url \n"}
-                    if (content.toUpperCase().contains('GROOVY'))
-                        edt {
-                            result.text += "A groovy site found: ${url} \n"
-                        }
+        hbox {
+            button('Search for Groovy', mnemonic:'S', actionPerformed: {
+                doOutside {
+                    urls.each {url ->
+                        edt {logMessages.text += "Started downloading from $url \n"}
+                        def content = url.toURL().text
+                        edt {logMessages.text += "Done downloading from $url \n"}
+                        if (content.toUpperCase().contains('GROOVY'))
+                            edt {
+                                result.text += "A groovy site found: ${url} \n"
+                            }
+                    }
                 }
-            }
-        })
+            })
+        }
     }
 }
 
