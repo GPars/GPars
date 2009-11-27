4f23d01260e67183d45412ebdeb8d8624d8e6cac
vaclav.pech <vaclav.pech@390304ce-b589-11dd-818f-3bd198e1d18e>
2009-11-27 18:34:16 +0000

    Added a dummy Java class to test a potential groovyc issue
    git-svn-id: https://gparallelizer.googlecode.com/svn/trunk@1002 390304ce-b589-11dd-818f-3bd198e1d18e

---
+++ src/test/groovy/groovyx/gpars/issues/ATest.java
@@ -0,0 +1,11 @@
+package groovyx.gpars.issues;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: vaclav
+ * Date: Nov 27, 2009
+ * Time: 7:30:13 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ATest {
+}
