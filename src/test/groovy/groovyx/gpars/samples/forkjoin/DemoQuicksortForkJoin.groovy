3604e39ac0a70e9b6afedb0fe30667acf5f4e835
vaclav.pech <vaclav.pech@390304ce-b589-11dd-818f-3bd198e1d18e>
2009-12-15 09:33:41 +0000

    Added F/J Quicksort demo
    git-svn-id: https://gparallelizer.googlecode.com/svn/trunk@1090 390304ce-b589-11dd-818f-3bd198e1d18e

---
+++ src/test/groovy/groovyx/gpars/samples/forkjoin/DemoQuicksortForkJoin.groovy
@@ -0,0 +1,66 @@
+//  GPars (formerly GParallelizer)
+//
+//  Copyright © 2008-9  The original author or authors
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License. 
+
+/**
+ * A parallel quicksort implementation based on Dierk Koenig's sample code kata.
+ *
+ * @author Vaclav Pech, Dierk Koenig
+ * Date: Dec 11, 2008
+ */
+
+package groovyx.gpars.samples.forkjoin
+
+import groovyx.gpars.AbstractForkJoinWorker
+import groovyx.gpars.Parallelizer
+import static groovyx.gpars.Parallelizer.doParallel
+
+@SuppressWarnings("GroovyMultipleReturnPointsPerMethod")
+class Sorter extends AbstractForkJoinWorker {
+
+    def index
+    def list
+
+    def Sorter(index, list) {
+        this.index = index
+        this.list = list
+    }
+
+    protected void compute() {
+        def groups = list.groupBy{it <=> list[list.size().intdiv(2)]}
+        if ((list.size() < 2) || (groups.size() == 1)) {
+            setResult([index:index, list:list.clone()])
+            return
+        }
+        (-1..1).each{forkOffChild new Sorter(it, groups[it]?:[])}
+        setResult([index:index, list:childrenResults.sort{it.index}.sum{it.list}])
+    }
+}
+
+def quicksort(list) {
+    doParallel {
+        Parallelizer.orchestrate(new Sorter(0, list)).list
+    }
+}
+
+assert quicksort([]) == []
+assert quicksort([1]) == [1]
+assert quicksort([1, 2, 3]) == [1, 2, 3]
+assert quicksort([3, 2, 1]) == [1, 2, 3]
+assert quicksort([3, 1, 2, 1]) == [1, 1, 2, 3]
+final def emptyList = []
+assert !quicksort(emptyList).is(emptyList)
+
+println quicksort([3, 20, 4, 6, 5, 7, 6, 2, 4, 3, 9, 0, 8, 7, 6, 7, 234, 545, 20, 24, 37, 2, 1])
