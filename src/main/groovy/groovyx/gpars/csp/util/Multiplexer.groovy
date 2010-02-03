e1f7f2f667071a245006f31efab4a7ca35aa2c58
Vaclav Pech <vaclav.pech@gmail.com>
2010-02-03 11:56:39 +0100

    Added the GPars license statement to CSP code

--- src/main/groovy/groovyx/gpars/csp/util/Multiplexer.groovy
+++ src/main/groovy/groovyx/gpars/csp/util/Multiplexer.groovy
@@ -1,20 +1,36 @@
+// GPars (formerly GParallelizer)
+//
+// Copyright © 2008-9  The original author or authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package groovyx.gpars.csp.util
 
+import groovyx.gpars.csp.ALT
+import groovyx.gpars.csp.ChannelInputList
 import org.jcsp.lang.CSProcess
 import org.jcsp.lang.ChannelOutput
-import groovyx.gpars.csp.ChannelInputList
-import groovyx.gpars.csp.ALT
 
 class Multiplexer implements CSProcess {
- 
-  def ChannelInputList inChannels
-  def ChannelOutput outChannel
 
-  void run () {  
-    def alt = new ALT( inChannels )
-    while (true) {
-      def index = alt.select ()
-      outChannel.write (inChannels[index].read())
-    } 
-  } 
+    def ChannelInputList inChannels
+    def ChannelOutput outChannel
+
+    void run() {
+        def alt = new ALT(inChannels)
+        while (true) {
+            def index = alt.select()
+            outChannel.write(inChannels[index].read())
+        }
+    }
 }
