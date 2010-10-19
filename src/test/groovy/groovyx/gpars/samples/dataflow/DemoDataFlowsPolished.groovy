8eb290bd22c201b715e9d23e2e7e97442c454bd3
Vaclav Pech <vaclav.pech@gmail.com>
2010-10-19 08:55:27 +0200

    Added a new df demo

---
+++ src/test/groovy/groovyx/gpars/samples/dataflow/DemoDataFlowsPolished.groovy
@@ -0,0 +1,39 @@
+// GPars - Groovy Parallel Systems
+//
+// Copyright © 2008-10  The original author or authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package groovyx.gpars.samples.dataflow
+
+import groovyx.gpars.dataflow.DataFlows
+import static groovyx.gpars.dataflow.DataFlow.task
+
+/**
+ * Demonstrates the use of the DataFlows class to exchange values among threads or tasks.
+ * Uses the with() method on DataFlows to simplify the DSL.
+ *
+ * @author Vaclav Pech, Dierk Koenig
+ */
+
+new DataFlows().with {
+    task { result = x + y }
+
+    task { x = 10 }
+
+    task { y = 5 }
+
+    assert 15 == result
+}
+
+System.exit 0
