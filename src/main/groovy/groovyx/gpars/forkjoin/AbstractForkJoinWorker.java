39cc5cc99a675388ffc98b6dd2e2539162c3fb2c
Vaclav Pech <vaclav.pech@gmail.com>
2011-03-07 22:12:52 +0100

    Ensured the children results are ordered based on their forks

--- src/main/groovy/groovyx/gpars/forkjoin/AbstractForkJoinWorker.java
+++ src/main/groovy/groovyx/gpars/forkjoin/AbstractForkJoinWorker.java
@@ -19,6 +19,7 @@ package groovyx.gpars.forkjoin;
 import jsr166y.RecursiveTask;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -59,7 +60,7 @@ public abstract class AbstractForkJoinWorker<T> extends RecursiveTask<T> {
      */
     protected final void forkOffChild(final AbstractForkJoinWorker<T> child) {
         if (children == null) children = new ArrayList<AbstractForkJoinWorker<T>>();
-        children.add(0, child);  //Forks and joins have to be processed in reverse order from one another
+        children.add(child);
         child.fork();
     }
 
@@ -68,12 +69,14 @@ public abstract class AbstractForkJoinWorker<T> extends RecursiveTask<T> {
      *
      * @return A list of results returned from the child tasks
      */
+    @SuppressWarnings({"unchecked"})
     public final List<T> getChildrenResults() {
         if (children == null) return Collections.emptyList();
-        final List<T> results = new ArrayList<T>(children.size());
-        for (final AbstractForkJoinWorker<T> worker : children) {
-            results.add(worker.join());
+        final T[] results = (T[]) new Object[children.size()];
+        for (int i = children.size() - 1; i >= 0; i--) {  //Forks and joins have to be processed in reverse order from one another
+            final AbstractForkJoinWorker<T> worker = children.get(i);
+            results[i] = worker.join();
         }
-        return results;
+        return Arrays.asList(results);
     }
 }
