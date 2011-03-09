c824547f9eab128c0764098bb72b3e7a830f2de3
Vaclav Pech <vaclav.pech@gmail.com>
2011-03-09 16:12:51 +0100

    Fixed actor test termination

--- src/test/groovy/groovyx/gpars/actor/nonBlocking/RepeatLoopTest.groovy
+++ src/test/groovy/groovyx/gpars/actor/nonBlocking/RepeatLoopTest.groovy
@@ -1,6 +1,6 @@
 // GPars - Groovy Parallel Systems
 //
-// Copyright © 2008-10  The original author or authors
+// Copyright © 2008-11  The original author or authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -29,7 +29,7 @@ public class RepeatLoopTest extends GroovyTestCase {
             loop {
                 if (count == 10) {
                     latch.countDown()
-                    stop()
+                    terminate()
                     return
                 }
                 count += 1
--- src/test/groovy/groovyx/gpars/actor/nonBlocking/TimeoutTest.groovy
+++ src/test/groovy/groovyx/gpars/actor/nonBlocking/TimeoutTest.groovy
@@ -1,6 +1,6 @@
 // GPars - Groovy Parallel Systems
 //
-// Copyright © 2008-10  The original author or authors
+// Copyright © 2008-11  The original author or authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -121,7 +121,7 @@ public class TimeoutTest extends GroovyTestCase {
                     react(1000) {
                         nestedCodeFlag.set(true)
                         nestedMessage = it
-                        stop()
+                        terminate()
                     }
                 }
             }
