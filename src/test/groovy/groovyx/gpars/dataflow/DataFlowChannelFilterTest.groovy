173ab8cb676b47bb61ede28e44a1e16a6f7e20e6
Vaclav Pech <vaclav.pech@gmail.com>
2011-03-30 15:25:16 +0200

    Added tests for channel filtering and value transformations

---
+++ src/test/groovy/groovyx/gpars/dataflow/DataFlowChannelFilterTest.groovy
@@ -0,0 +1,65 @@
+// GPars - Groovy Parallel Systems
+//
+// Copyright © 2008-11  The original author or authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package groovyx.gpars.dataflow
+
+import static groovyx.gpars.dataflow.DataFlow.operator
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Vaclav
+ * Date: 30.3.11
+ * Time: 14:28
+ * To change this template use File | Settings | File Templates.
+ */
+class DataFlowChannelFilterTest extends GroovyTestCase {
+
+    public void testCreationFromQueue() {
+        final DataFlowQueue queue = new DataFlowQueue()
+        final DataFlowReadChannel filteredQueue = new DataFlowQueue()
+        operator(queue, filteredQueue) {
+            if (it < 5) bindOutput it
+        }
+        queue.bind 10
+        queue.bind 20
+        queue.bind 1
+        queue.bind 2
+        queue.bind 5
+        queue.bind 3
+        assert 1 == filteredQueue.val
+        assert 2 == filteredQueue.val
+        assert 3 == filteredQueue.val
+        assert !filteredQueue.isBound()
+    }
+
+    public void testCreationFromBroadCast() {
+        final DataFlowBroadcast queue = new DataFlowBroadcast()
+        final DataFlowReadChannel filteredQueue = new DataFlowQueue()
+        operator(queue.createReadChannel(), filteredQueue) {
+            if (it < 5) bindOutput it
+        }
+        queue.bind 10
+        queue.bind 20
+        queue.bind 1
+        queue.bind 2
+        queue.bind 5
+        queue.bind 3
+        assert 1 == filteredQueue.val
+        assert 2 == filteredQueue.val
+        assert 3 == filteredQueue.val
+        assert !filteredQueue.isBound()
+    }
+}
