49741d1a3ff1cae6bd0e893a0811be2fafc5e6d4
Vaclav Pech <vaclav.pech@gmail.com>
2011-05-12 12:13:44 +0200

    Testing return types of active methods

---
+++ src/test/groovy/groovyx/gpars/activeobject/NonBlockingTypesTest.groovy
@@ -0,0 +1,178 @@
+// GPars - Groovy Parallel Systems
+//
+// Copyright © 2008-11  The original author or authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package groovyx.gpars.activeobject
+
+import org.codehaus.groovy.control.MultipleCompilationErrorsException
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Vaclav
+ * Date: 12.5.11
+ * Time: 9:19
+ * To change this template use File | Settings | File Templates.
+ */
+class NonBlockingTypesTest extends GroovyTestCase {
+    public void testDefault() {
+        final GroovyShell shell = new GroovyShell()
+        def (a) = shell.evaluate("""
+import groovyx.gpars.activeobject.ActiveObject
+import groovyx.gpars.activeobject.ActiveMethod
+import groovyx.gpars.dataflow.Promise
+import groovyx.gpars.dataflow.DataflowVariable
+
+@ActiveObject
+class A {
+    @ActiveMethod
+    def foo() {
+        return 10
+    }
+}
+
+[new A()]
+""")
+        assert 10 == a.foo().get()
+    }
+
+    public void testObject() {
+        final GroovyShell shell = new GroovyShell()
+        def (a) = shell.evaluate("""
+import groovyx.gpars.activeobject.ActiveObject
+import groovyx.gpars.activeobject.ActiveMethod
+import groovyx.gpars.dataflow.Promise
+import groovyx.gpars.dataflow.DataflowVariable
+
+@ActiveObject
+class A {
+    @ActiveMethod
+    Object foo() {
+        return 10
+    }
+}
+
+[new A()]
+""")
+        assert 10 == a.foo().get()
+    }
+
+    public void testVoid() {
+        final GroovyShell shell = new GroovyShell()
+        def (a) = shell.evaluate("""
+import groovyx.gpars.activeobject.ActiveObject
+import groovyx.gpars.activeobject.ActiveMethod
+import groovyx.gpars.dataflow.Promise
+import groovyx.gpars.dataflow.DataflowVariable
+
+@ActiveObject
+class A {
+    @ActiveMethod
+    void foo() {
+        return
+    }
+}
+
+[new A()]
+""")
+        assert null == a.foo()
+    }
+
+    public void testDataflowVariable() {
+        final GroovyShell shell = new GroovyShell()
+        def (a) = shell.evaluate("""
+import groovyx.gpars.activeobject.ActiveObject
+import groovyx.gpars.activeobject.ActiveMethod
+import groovyx.gpars.dataflow.Promise
+import groovyx.gpars.dataflow.DataflowVariable
+
+@ActiveObject
+class A {
+    @ActiveMethod
+    DataflowVariable foo() {
+        return 10
+    }
+}
+
+[new A()]
+""")
+        assert 10 == a.foo().get()
+    }
+
+    public void testDataflowPromise() {
+        final GroovyShell shell = new GroovyShell()
+        def (a) = shell.evaluate("""
+import groovyx.gpars.activeobject.ActiveObject
+import groovyx.gpars.activeobject.ActiveMethod
+import groovyx.gpars.dataflow.Promise
+import groovyx.gpars.dataflow.DataflowVariable
+
+@ActiveObject
+class A {
+    @ActiveMethod
+    Promise<Integer> foo() {
+        return 10
+    }
+}
+
+[new A()]
+""")
+        assert 10 == a.foo().get()
+    }
+
+    public void testDataflowInteger() {
+        final GroovyShell shell = new GroovyShell()
+        shouldFail(MultipleCompilationErrorsException) {
+            def (a) = shell.evaluate("""
+    import groovyx.gpars.activeobject.ActiveObject
+    import groovyx.gpars.activeobject.ActiveMethod
+    import groovyx.gpars.dataflow.Promise
+    import groovyx.gpars.dataflow.DataflowVariable
+
+    @ActiveObject
+    class A {
+        @ActiveMethod
+        Integer foo() {
+            return 10
+        }
+    }
+
+    [new A()]
+    """)
+        }
+    }
+
+    public void testDataflowInt() {
+        final GroovyShell shell = new GroovyShell()
+        shouldFail(MultipleCompilationErrorsException) {
+            def (a) = shell.evaluate("""
+    import groovyx.gpars.activeobject.ActiveObject
+    import groovyx.gpars.activeobject.ActiveMethod
+    import groovyx.gpars.dataflow.Promise
+    import groovyx.gpars.dataflow.DataflowVariable
+
+    @ActiveObject
+    class A {
+        @ActiveMethod
+        int foo() {
+            return 10
+        }
+    }
+
+    [new A()]
+    """)
+        }
+    }
+
+}
