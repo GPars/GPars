0fa119fdb704c6bcc53f34c48c8fc358ab1ba01f
Hamlet D'Arcy <hamletdrc@gmail.com>
2011-05-14 12:42:45 +0200

    Basic implementation of an @AsyncFun annotation, which is a replacement for the .asyncFun() method call on field definitions.

---
+++ src/main/groovy/groovyx/gpars/AsyncFun.groovy
@@ -0,0 +1,39 @@
+// GPars - Groovy Parallel Systems
+//
+// Copyright © 2008--2011  The original author or authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package groovyx.gpars
+
+import org.codehaus.groovy.transform.GroovyASTTransformationClass
+import java.lang.annotation.Retention
+import java.lang.annotation.RetentionPolicy
+import java.lang.annotation.Target
+import java.lang.annotation.ElementType
+import groovyx.gpars.util.AsyncFunASTTransformation
+
+/**
+ * This annotation makes a field or local variable as an asynchronous function, and the field/variable should be
+ * evaluated only within the context of a ThreadPool.
+ *
+ * @author Vladimir Orany
+ * @author Hamlet D'Arcy
+ * Date: May 14, 2011
+ */
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target([ElementType.FIELD])
+@GroovyASTTransformationClass(classes = [AsyncFunASTTransformation])
+@interface AsyncFun {
+}
---
+++ src/test/groovy/groovyx/gpars/AsyncFunAnnotationTest.groovy
@@ -0,0 +1,100 @@
+// GPars - Groovy Parallel Systems
+//
+// Copyright © 2008--2011  The original author or authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package groovyx.gpars
+
+import groovyx.gpars.dataflow.Promise
+import spock.lang.Specification
+
+class AsyncFunAnnotationTest extends Specification {
+
+    def "test basic annotation usage"() {
+
+        when:
+        boolean wasCalled = false
+        groovyx.gpars.GParsPool.withPool(5) {
+            def tester = new TestSum()
+            assert tester.sum(1, 2).val == 3
+            assert tester.sum(10, 15) instanceof Promise
+            wasCalled = true
+        }
+
+        then:
+        wasCalled
+    }
+
+    def "test annotation on already annotated field value"() {
+
+        when:
+        boolean wasCalled = false
+        groovyx.gpars.GParsPool.withPool(5) {
+            def tester = new TestSum()
+            assert tester.sum2(1, 2).val == 3
+            assert tester.sum2(10, 15) instanceof Promise
+            wasCalled = true
+        }
+
+        then:
+        wasCalled
+    }
+
+    def "test annotation on method expression"() {
+
+        when:
+        boolean wasCalled = false
+        groovyx.gpars.GParsPool.withPool(5) {
+            def tester = new TestSum()
+            assert tester.sum3(1, 2).val == 3
+            assert tester.sum3(10, 15) instanceof Promise
+            wasCalled = true
+        }
+
+        then:
+        wasCalled
+    }
+
+    def "test ast transformation implementation"() {
+        when:
+        boolean x = true
+        then:
+        new GroovyShell().evaluate("""
+            class MyClass {
+                @groovyx.gpars.AsyncFun
+                def f = { true }
+            }
+            true
+""")
+    }
+
+    class TestSum {
+        @AsyncFun
+        def sum = {a, b -> a + b }
+
+        @AsyncFun
+        def sum2 = getClosureSum()
+
+        @AsyncFun
+        def sum3 = getNewClosureSum()
+
+        def getClosureSum(){
+            sum
+        }
+
+        def getNewClosureSum() {
+            { a, b -> a + b }
+        }
+    }
+}
