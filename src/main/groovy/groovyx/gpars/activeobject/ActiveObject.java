aea6bb4ba7ece83ac0a2a46c46db2936c50e54c9
Hamlet D'Arcy <hamletdrc@gmail.com>
2011-05-14 18:02:11 +0200

    fixed ActiveObject, which was missing an import.

--- src/main/groovy/groovyx/gpars/activeobject/ActiveObject.java
+++ src/main/groovy/groovyx/gpars/activeobject/ActiveObject.java
@@ -22,6 +22,7 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.lang.annotation.Documented;
 
 @Documented
 @Retention(RetentionPolicy.SOURCE)
