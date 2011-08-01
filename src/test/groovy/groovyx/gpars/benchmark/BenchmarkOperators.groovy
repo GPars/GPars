e71ddbc0bad1e132cb298495d5210018c789d522
Vaclav Pech <vaclav.pech@gmail.com>
2011-08-01 08:36:29 +0200

    Added a benchmark for df operators

---
+++ src/test/groovy/groovyx/gpars/benchmark/BenchmarkOperators.groovy
@@ -0,0 +1,50 @@
+// GPars - Groovy Parallel Systems
+//
+// Copyright © 2008-11  The original author or authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package groovyx.gpars.benchmark
+
+import groovyx.gpars.dataflow.DataflowQueue
+import groovyx.gpars.group.DefaultPGroup
+import groovyx.gpars.scheduler.FJPool
+import java.util.concurrent.CountDownLatch
+
+final def concurrencyLevel = 8
+group = new DefaultPGroup(new FJPool(concurrencyLevel))
+
+final DataflowQueue queue = new DataflowQueue()
+final CountDownLatch latch = new CountDownLatch(1)
+(1..100).each {
+    queue << it
+}
+queue << -1
+
+final def t1 = System.currentTimeMillis()
+
+int sum = 0
+group.operator([queue], []) {
+    if (it == -1) {
+        println sum
+        terminate()
+        latch.countDown()
+    } else {
+        sum += it
+    }
+}
+
+latch.await()
+group.shutdown()
+final def t2 = System.currentTimeMillis()
+println(t2 - t1)
