9627807ced8be947fec14f4310a49eed0d557acd
Vaclav Pech <vaclav.pech@gmail.com>
2011-08-01 15:07:41 +0200

    DataflowPrioritySelector migrated into Java

--- src/main/groovy/groovyx/gpars/dataflow/operator/DataflowPrioritySelector.java
+++ src/main/groovy/groovyx/gpars/dataflow/operator/DataflowPrioritySelector.java
@@ -14,9 +14,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package groovyx.gpars.dataflow.operator
+package groovyx.gpars.dataflow.operator;
 
-import groovyx.gpars.group.PGroup
+import groovy.lang.Closure;
+import groovyx.gpars.group.PGroup;
+
+import java.util.Map;
 
 /**
  * Dataflow selectors and operators (processors) form the basic units in dataflow networks. They are typically combined into oriented graphs that transform data.
@@ -24,28 +27,29 @@ import groovyx.gpars.group.PGroup
  * of the input channels the selector's body is triggered on the values, potentially generating values to be written into the output channels.
  * The output channels at the same time are suitable to be used as input channels by some other dataflow processors.
  * The channels allow processors to communicate.
- *
+ * <p/>
  * Dataflow selectors and operators enable creation of highly concurrent applications yet the abstraction hides the low-level concurrency primitives
  * and exposes much friendlier API.
  * Since selectors and operators internally leverage the actor implementation, they reuse a pool of threads and so the actual number of threads
  * used by the calculation can be kept much lower than the actual number of processors used in the network.
- *
+ * <p/>
  * Priority selectors prefer to select messages from channels with lower position index.
  *
  * @author Vaclav Pech
- * Date: Sep 23, 2009
+ *         Date: Sep 23, 2009
  */
 public final class DataflowPrioritySelector extends DataflowSelector {
 
     /**
      * Creates a priority selector
      * After creation the selector needs to be started using the start() method.
-     * @param group A parallel group to use threads from in the internal actor
+     *
+     * @param group    A parallel group to use threads from in the internal actor
      * @param channels A map specifying "inputs" and "outputs" - dataflow channels (instances of the DataflowQueue or DataflowVariable classes) to use for inputs and outputs
-     * @param code The selector's body to run each time all inputs have a value to read
+     * @param code     The selector's body to run each time all inputs have a value to read
      */
-    def DataflowPrioritySelector(final PGroup group, final Map channels, final Closure code) {
-        super(group, channels, code)
+    DataflowPrioritySelector(final PGroup group, final Map<String, Object> channels, final Closure code) {
+        super(group, channels, code);
     }
 
     /**
@@ -53,8 +57,12 @@ public final class DataflowPrioritySelector extends DataflowSelector {
      * The selector's guards are applied to the selection.
      */
     @Override
-    void doSelect() {
-        select.prioritySelect(this.actor, guards)
+    public void doSelect() {
+        try {
+            select.prioritySelect(this.actor, guards);
+        } catch (InterruptedException e) {
+            throw new IllegalStateException("Couldn't not select a value due to an exception.", e);
+        }
     }
 }
 
