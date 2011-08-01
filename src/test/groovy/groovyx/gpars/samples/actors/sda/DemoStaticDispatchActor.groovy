1dbc63c638b31f1939f168496e7563d4c27f3b2f
Vaclav Pech <vaclav.pech@gmail.com>
2011-08-01 15:23:01 +0200

    Added demos for SDA

---
+++ src/test/groovy/groovyx/gpars/samples/actors/sda/DemoStaticDispatchActor.groovy
@@ -0,0 +1,39 @@
+// GPars - Groovy Parallel Systems
+//
+// Copyright © 2008-11  The original author or authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package groovyx.gpars.samples.actors.sda
+
+import groovyx.gpars.actor.StaticDispatchActor
+
+/**
+ * Demonstrates use of the StaticDispatchActor class, which offers quick actor implementation leveraging compile-time binding of the message handler.
+ */
+
+final class MyActor extends StaticDispatchActor<String> {
+
+    void onMessage(String message) {
+        println 'Received string ' + message
+        if (message == 'hello') reply 'Hi!'
+        if (message == 'stop') stop()
+    }
+}
+
+final def actor = new MyActor().start()
+
+println 'Reply: ' + actor.sendAndWait('hello')
+actor 'bye'
+actor 'stop'
+actor.join()
