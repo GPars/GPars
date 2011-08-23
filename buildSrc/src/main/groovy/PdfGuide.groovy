e2a6ccbf33f3eadf83494c7bc62f205a778bcbd7
Russel Winder <russel@russel.org.uk>
2011-08-23 09:50:33 +0100

    Parameterize the build directory as best as can.

--- buildSrc/src/main/groovy/PdfGuide.groovy
+++ buildSrc/src/main/groovy/PdfGuide.groovy
@@ -1,3 +1,25 @@
+/*
+ * Copyright 2010-2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ *  @author <who were the original authors?
+ *
+ *  @author Russel Winder
+ */
+
 import org.gradle.api.file.FileCollection
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.InputFiles
@@ -7,12 +29,16 @@ import org.gradle.api.tasks.TaskAction
 import grails.doc.PdfBuilder
 
 class PdfGuide extends DefaultTask {
+
+  //  Ensure consistency with path in docs.gradle
+  final String guideBuildDirectory = "${project.buildDir}/manual"
+
     @OutputDirectory File outputFile
 	@Input String pdfName
 	
 	public PdfGuide(){
 		super()
-		outputFile = new File("${project.buildDir}/manual")
+		outputFile = new File(guideBuildDirectory)
 	}
 	
 	@TaskAction
@@ -29,7 +55,7 @@ class PdfGuide extends DefaultTask {
             // ignore for now
         }
     	
-		project.file("${project.buildDir}/manual/guide/single.pdf")
+		project.file(guideBuildDirectory + '/guide/single.pdf')
 			   .renameTo(new File(outputFile.absolutePath, pdfName).absolutePath)
 	}
 }
