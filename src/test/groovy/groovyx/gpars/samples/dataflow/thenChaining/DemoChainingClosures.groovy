811d3bb564a97472835014ae2c9b6e4fe14bedf9
Vaclav Pech <vaclav.pech@gmail.com>
2011-11-24 17:11:19 +0100

    Renamed a demo

---
+++ src/test/groovy/groovyx/gpars/samples/dataflow/thenChaining/DemoChainingClosures.groovy
@@ -0,0 +1,30 @@
+// GPars - Groovy Parallel Systems
+//
+// Copyright © 2008-11  The original author or authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package groovyx.gpars.samples.dataflow.thenChaining
+
+import groovyx.gpars.dataflow.DataflowVariable
+
+/**
+ * Uses the Promise.then() method to chain multiple closures.
+ */
+
+final DataflowVariable variable = new DataflowVariable()
+final DataflowVariable result = new DataflowVariable()
+
+variable.then {it * 2} then {it + 1} then {result << it}
+variable << 4
+assert 9 == result.val
