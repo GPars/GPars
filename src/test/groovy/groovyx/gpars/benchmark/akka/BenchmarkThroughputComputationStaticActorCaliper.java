deb7e40bf0ac83cf33620571bbf5e8d1030b3ee1
Kevin Chen <kevinwchen26@gmail.com>
2012-07-05 16:52:57 -0500

    Benchmark is now much shorter

--- src/test/groovy/groovyx/gpars/benchmark/akka/BenchmarkThroughputComputationStaticActorCaliper.java
+++ src/test/groovy/groovyx/gpars/benchmark/akka/BenchmarkThroughputComputationStaticActorCaliper.java
@@ -28,11 +28,11 @@ import java.util.ArrayList;
 import java.util.concurrent.CountDownLatch;
 
 public class BenchmarkThroughputComputationStaticActorCaliper extends Benchmark {
-    @Param({"1", "2", "4", "6", "8",
+    @Param({"1", "2", "4"/*, "6", "8",
             "10", "12", "14", "16", "18",
             "20", "22", "24", "26", "28",
             "30", "32", "34", "36", "38",
-            "40", "42", "44", "46", "48"}
+            "40", "42", "44", "46", "48"*/}
     )
     int numberOfClients;
 
