7806ec9e268ef2ff36a24d604dd2ca828ac5b27d
Vaclav Pech <vaclav.pech@gmail.com>
2012-09-10 23:29:24 +0200

    Unified moveHead and moveAsyncHead

--- src/main/groovy/groovyx/gpars/dataflow/stream/DataflowStreamReadAdapter.java
+++ src/main/groovy/groovyx/gpars/dataflow/stream/DataflowStreamReadAdapter.java
@@ -1,12 +1,12 @@
 // GPars - Groovy Parallel Systems
 //
-// Copyright © 2008-11  The original author or authors
+// Copyright © 2008-2012  The original author or authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-// http://www.apache.org/licenses/LICENSE-2.0
+//       http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -442,12 +442,12 @@ public class DataflowStreamReadAdapter<T> implements DataflowReadChannel<T> {
     }
 
     private void moveHead() {
-        if (head == asyncHead) moveAsyncHead();
+        asyncHead = (StreamCore<T>) asyncHead.getRest();
         head = (StreamCore<T>) head.getRest();
     }
 
     private void moveAsyncHead() {
-        asyncHead = (StreamCore<T>) asyncHead.getRest();
+        moveHead();
     }
 }
 
