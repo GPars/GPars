b6184139a5d0e6d70201a4b6c739b3119fa24c84
Vaclav Pech <vaclav.pech@gmail.com>
2013-07-05 15:57:29 +0200

    Updated the new Salact demos

---
+++ src/test/groovy/groovyx/gpars/samples/dataflow/select/DemoTheWinnerTakesItAllTheLoserStandingSmall.groovy
@@ -0,0 +1,56 @@
+// GPars - Groovy Parallel Systems
+//
+// Copyright © 2008-2012  The original author or authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package groovyx.gpars.samples.dataflow.select
+
+import groovyx.gpars.dataflow.Promise
+import groovyx.gpars.dataflow.Select
+import groovyx.gpars.group.DefaultPGroup
+
+import java.util.concurrent.atomic.AtomicBoolean
+
+/**
+ * Demonstrates the use of dataflow tasks and selects to pick the fastest result of concurrently run calculations.
+ * It shows a waz to cancel the slower tasks once a result is known
+ */
+
+final group = new DefaultPGroup()
+final done = new AtomicBoolean()
+
+group.with {
+    Promise p1 = task {
+        sleep(1000)
+        if (done.get()) return
+        10 * 10 + 1
+    }
+    Promise p2 = task {
+        sleep(1000)
+        if (done.get()) return
+        5 * 20 + 2
+    }
+    Promise p3 = task {
+        sleep(1000)
+        if (done.get()) return
+        1 * 100 + 3
+    }
+
+    final alt = new Select(group, p1, p2, p3, Select.createTimeout(500))
+    def result = alt.select()
+    done.set(true)
+    println "Result: " + result
+}
+sleep 3000
+group.shutdown()
