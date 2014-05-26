86cec37538a5ecf5e9bdea0e254ff657af6cb959
Rafał Sławik <rafal.slawik@gmail.com>
2014-05-26 14:20:11 +0200

    NettyServer, NettyClient test

---
+++ src/test/groovy/groovyx/gpars/remote/netty/NettyClientServerTest.groovy
@@ -0,0 +1,32 @@
+// GPars - Groovy Parallel Systems
+//
+// Copyright © 2014 The original author or authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package groovyx.gpars.remote.netty
+
+class NettyClientServerTest extends GroovyTestCase {
+
+    public void testConnectionLocal() {
+        // start server on localhost
+        NettyServer server = new NettyServer();
+
+        //start client on localhost
+        // NettyClient ...
+
+        // client connects to server
+
+        // client sends message
+    }
+}
