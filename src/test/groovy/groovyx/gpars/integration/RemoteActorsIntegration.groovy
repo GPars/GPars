2b929d1874a5766819fd2474d790737ad83e4324
Rafał Sławik <rafal.slawik@gmail.com>
2014-06-04 17:29:25 +0200

    Remainders

---
+++ src/test/groovy/groovyx/gpars/integration/RemoteActorsIntegration.groovy
@@ -0,0 +1,8 @@
+package groovyx.gpars.integration
+
+public class RemoteActorsIntegrations extends GroovyTestCase {
+
+    void testRemoteActors() {
+        assert fail("Write tests here!")
+    }
+}
