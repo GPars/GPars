51ee59105651923b1172425617b2093c504bdf2a
Rafał Sławik <rafal.slawik@gmail.com>
2014-07-02 21:58:54 +0200

    DataflowVariable can be published

--- src/main/groovy/groovyx/gpars/dataflow/remote/RemoteDataflows.java
+++ src/main/groovy/groovyx/gpars/dataflow/remote/RemoteDataflows.java
@@ -1,4 +1,18 @@
 package groovyx.gpars.dataflow.remote;
 
+import groovyx.gpars.dataflow.DataflowVariable;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class RemoteDataflows {
+    private static Map<String, DataflowVariable<?>> publishedVariables = new ConcurrentHashMap<>();
+
+    public static <T> void publish(DataflowVariable<T> variable, String name) {
+        publishedVariables.put(name, variable);
+    }
+
+    public static DataflowVariable<?> get(String name) {
+        return publishedVariables.get(name);
+    }
 }
--- src/test/groovy/groovyx/gpars/dataflow/remote/RemoteDataflowsTest.groovy
+++ src/test/groovy/groovyx/gpars/dataflow/remote/RemoteDataflowsTest.groovy
@@ -1,6 +1,19 @@
 package groovyx.gpars.dataflow.remote
 
+import groovyx.gpars.dataflow.DataflowVariable
 import spock.lang.Specification
 
 class RemoteDataflowsTest extends Specification {
+
+    def "can publish DataflowVariable"() {
+        setup:
+        DataflowVariable<String> var = new DataflowVariable<>()
+        def varName = "test-variable"
+
+        when:
+        RemoteDataflows.publish var, varName
+
+        then:
+        RemoteDataflows.get varName
+    }
 }
