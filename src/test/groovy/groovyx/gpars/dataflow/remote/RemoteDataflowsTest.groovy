7a8265f803196904650bb0ee9fe77e959a585763
Rafał Sławik <rafal.slawik@gmail.com>
2014-07-07 13:06:24 +0200

    Retrieving DataflowVariable from remote host - test.

--- src/test/groovy/groovyx/gpars/dataflow/remote/RemoteDataflowsTest.groovy
+++ src/test/groovy/groovyx/gpars/dataflow/remote/RemoteDataflowsTest.groovy
@@ -1,9 +1,13 @@
 package groovyx.gpars.dataflow.remote
 
 import groovyx.gpars.dataflow.DataflowVariable
+import groovyx.gpars.remote.netty.NettyTransportProvider
 import spock.lang.Specification
+import spock.lang.Timeout
 
 class RemoteDataflowsTest extends Specification {
+    def static HOST = "localhost"
+    def static PORT = 9021
 
     def "retrieving not published DataflowVariable returns null"() {
         when:
@@ -27,12 +31,10 @@ class RemoteDataflowsTest extends Specification {
 
     def "retrieving DataflowVariable from remote host returns Future"() {
         setup:
-        def host = "dummy-host"
-        def port = 12345 // dummy port
         def varName = "test-variable"
 
         when:
-        def varFuture = RemoteDataflows.get host, port, varName
+        def varFuture = RemoteDataflows.get HOST, PORT, varName
 
         then:
         varFuture != null
@@ -41,17 +43,33 @@ class RemoteDataflowsTest extends Specification {
 
     def "retrieving DataflowVariable from remote host returns Future based on the same inner variable"() {
         setup:
-        def host = "dummy-host"
-        def port = 12345 // dummy port
         def varName = "test-variable"
 
         when:
-        def varFuture1 = RemoteDataflows.get host, port, varName
-        def varFuture2 = RemoteDataflows.get host, port, varName
+        def varFuture1 = RemoteDataflows.get HOST, PORT, varName
+        def varFuture2 = RemoteDataflows.get HOST, PORT, varName
 
         then:
         varFuture1 != varFuture2
         varFuture1.remoteVariable != null
         varFuture1.remoteVariable == varFuture2.remoteVariable
+    }
+
+    @Timeout(5)
+    def "can retrieve published DataflowVariable"() {
+        setup:
+        def variableName = "test-variable"
+        def variable = new DataflowVariable<String>()
+        def testValue = "test DataflowVariable"
+
+        when:
+        NettyTransportProvider.startServer HOST, PORT
+        RemoteDataflows.publish variable, variableName
+        def remoteVariable = RemoteDataflows.get HOST, PORT, variableName get()
+
+        variable << testValue
+
+        then:
+        remoteVariable.val == testValue
     }
 }
