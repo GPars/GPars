6635066a8f1795a7df72c3fbaff8baad9d385a5b
Rafał Sławik <rafal.slawik@gmail.com>
2014-08-03 13:46:24 +0200

    Dataflow variable can be published and remotely retrieved after bind.

--- src/main/groovy/groovyx/gpars/remote/message/RemoteDataflowVariableReplyMsg.java
+++ src/main/groovy/groovyx/gpars/remote/message/RemoteDataflowVariableReplyMsg.java
@@ -9,16 +9,27 @@ public class RemoteDataflowVariableReplyMsg extends SerialMsg {
 
     private final String name;
     private final DataflowVariable variable;
+    private Object value;
 
     public RemoteDataflowVariableReplyMsg(String name, DataflowVariable variable) {
         this.name = name;
         this.variable = variable;
+        if (variable.isBound()) {
+            try {
+                value = variable.getVal();
+            } catch (InterruptedException e) {
+                // fail silently
+            }
+        }
     }
 
     @Override
     public void execute(RemoteConnection conn) {
         DataflowVariable remoteVariable = conn.getLocalHost().getRemoteDataflowsRegistry().get(name);
         remoteVariable.bindUnique(variable);
+        if (value != null) {
+            variable.bindUnique(value);
+        }
     }
 
 }
--- src/test/groovy/groovyx/gpars/samples/remote/dataflow/variable/ExampleVariableServer.groovy
+++ src/test/groovy/groovyx/gpars/samples/remote/dataflow/variable/ExampleVariableServer.groovy
@@ -8,6 +8,8 @@ import groovyx.gpars.remote.netty.NettyTransportProvider
 def HOST = "localhost"
 def PORT = 9009
 
+println "Example: DataflowVariable"
+
 LocalHost localHost = new LocalHost()
 NettyTransportProvider.startServer HOST, PORT, localHost
 
@@ -17,10 +19,11 @@ def var2 = new DataflowVariable()
 RemoteDataflows.publish var1, "variabledemo-var-GPars"
 RemoteDataflows.publish var2, "variabledemo-var-Dataflows"
 
+println "Before bind"
 sleep 10000
-
 var1 << "GPars"
 var2 << "Remote Dataflows"
+println "After bind"
 
 // NettyTransportProvider.stopServer()
 
