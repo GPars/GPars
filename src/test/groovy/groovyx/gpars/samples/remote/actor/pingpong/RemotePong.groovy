56cb16beddcdad3aab93820e959c6cb37e0b5d16
Rafał Sławik <rafal.slawik@gmail.com>
2014-08-04 21:29:09 +0200

    RemoteActors with new API. Working Ping-Pong example

--- src/test/groovy/groovyx/gpars/samples/remote/actor/pingpong/RemotePong.groovy
+++ src/test/groovy/groovyx/gpars/samples/remote/actor/pingpong/RemotePong.groovy
@@ -4,6 +4,9 @@ import groovyx.gpars.actor.Actors
 import groovyx.gpars.actor.remote.RemoteActors
 import groovyx.gpars.remote.netty.NettyTransportProvider
 
+def HOST = "localhost"
+def PORT = 9000
+
 def pongActor = Actors.actor {
     println "Pong Actor"
 
@@ -16,8 +19,10 @@ def pongActor = Actors.actor {
     }
 }
 
-NettyTransportProvider.startServer("localhost", 9000)
-// register pongActor as a remote actor
-RemoteActors.register(pongActor, "pong")
+def remoteActors = RemoteActors.create()
+remoteActors.startServer HOST, PORT
+
+// publish pongActor as a remote actor
+remoteActors.publish pongActor, "pong"
 
 pongActor.join()
