733f57dc6f1ec78aeb8477c4adf7dc053e27a236
Kirill Vergun <code@o-nix.me>
2014-11-19 19:37:17 +0200

    Fixed some mistakes.

--- src/main/groovy/groovyx/gpars/activeobject/ActorWithExceptionHandler.java
+++ src/main/groovy/groovyx/gpars/activeobject/ActorWithExceptionHandler.java
@@ -4,6 +4,6 @@ package groovyx.gpars.activeobject;
  * @author Kirill Vergun <code@o-nix.me>
  * @since 14.11.14
  */
-interface ActorWithExceptionHandler {
+public interface ActorWithExceptionHandler {
     Object recoverFromException(String methodName, Exception e);
 }
