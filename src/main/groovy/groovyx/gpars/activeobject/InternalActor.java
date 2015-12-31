1f4fdb20634e69bc793dae8f0f995527f132ed97
Vaclav Pech <vaclav.pech@gmail.com>
2015-12-31 22:48:21 +0100

    Update the CI inspection profiles, spellchecker dictionary and fix a few spelling errors

--- src/main/groovy/groovyx/gpars/activeobject/InternalActor.java
+++ src/main/groovy/groovyx/gpars/activeobject/InternalActor.java
@@ -1,12 +1,12 @@
 // GPars - Groovy Parallel Systems
 //
-// Copyright © 2008-11  The original author or authors
+// Copyright © 2008-2013  The original author or authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-// http://www.apache.org/licenses/LICENSE-2.0
+//       http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -62,10 +62,7 @@ public final class InternalActor extends AbstractLoopingActor {
     public static InternalActor create(final Object groupId) {
         final PGroup group;
 
-        if ("".equals(groupId))
-            group = Actors.defaultActorPGroup;
-        else
-            group = ActiveObjectRegistry.getInstance().findGroupById((String) groupId);
+        group = "".equals(groupId) ? Actors.defaultActorPGroup : ActiveObjectRegistry.getInstance().findGroupById((String) groupId);
 
         if (group == null)
             throw new IllegalArgumentException("Cannot find a PGroup "
@@ -84,11 +81,7 @@ public final class InternalActor extends AbstractLoopingActor {
     private static Object handleCurrentMessage(final Object msg) {
         try {
             final Object[] params;
-            if (msg instanceof Collection) {
-                params = ((Collection<Object>) msg).toArray(new Object[((Collection<Object>) msg).size()]);
-            } else {
-                params = (Object[]) msg;
-            }
+            params = msg instanceof Collection ? ((Collection<Object>) msg).toArray(new Object[((Collection<Object>) msg).size()]) : (Object[]) msg;
 
             final Object target = params[0];
             final String methodName = (String) params[1];
